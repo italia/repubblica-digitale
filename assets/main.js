cc9809a80c49f15d3313e143f227de5b770cc992
marco <webgrafia@gmail.com>
2020-05-26 13:11:43 +0200

    Ricerca multipla

--- assets/main.js
+++ assets/main.js
@@ -54,12 +54,13 @@ $(function () {
 
 
 var Shuffle = window.Shuffle;
-
+var SearchKey = [];
 class Iniziative {
     constructor(element) {
         this.element = element;
         this.shuffle = new Shuffle(element, {
-            itemSelector: '.card-iniziativa'
+            itemSelector: '.card-iniziativa',
+            filterMode: Shuffle.FilterMode.ALL
         });
 
         // Log events.
@@ -99,7 +100,57 @@ class Iniziative {
         this._removeActiveClassFromChildren(options);
         document.querySelector('#selected-filter').innerHTML = "";
         this.shuffle.filter(Shuffle.ALL_ITEMS);
+        SearchKey = [];
 
+    }
+
+
+    removeLabel(label){
+        var pos = SearchKey.indexOf(label);
+        SearchKey.splice(pos, 1);
+        this.shuffle.filter(SearchKey);
+        this.stampaFiltri();
+
+        const options = document.querySelectorAll('.filter-options .btn');
+        this._removeActiveClassFromChildren(options);
+        this._addActiveClass(options);
+
+    }
+
+    stampaFiltri(){
+        document.querySelector('#selected-filter').innerHTML = "";
+
+        if(SearchKey.length > 0){
+            document.querySelector('#selected-filter').insertAdjacentHTML(
+                'afterbegin',
+                'Stai visualizzando soltanto le iniziative che hanno i seguenti filtri: <br>\n'
+            );
+
+
+            SearchKey.forEach(function(item){
+                document.querySelector('#selected-filter').insertAdjacentHTML(
+                    'beforeend',
+                    ' <div class="chip chip-lg chip-primary mt-3"  onclick="window.iniziative.removeLabel(\'' + item + '\')">\n' +
+                    '      <span class="chip-label">' + item + '</span>\n' +
+                    '      <button>\n' +
+                    '        <svg class="icon"><use xlink:href="/assets/bootstrap-italia/dist/svg/sprite.svg#it-close"></use></svg>\n' +
+                    '        <span class="sr-only">Elimina ' + item + '</span>\n' +
+                    '      </button>\n' +
+                    '    </div>  '
+                )
+
+            });
+            document.querySelector('#selected-filter').insertAdjacentHTML(
+                'beforeend',
+                '<a class="read-more mt-3" href="#"  onclick="window.iniziative.removeLabels()">\n' +
+                '   <span class="text">Rimuovi tutti i filtri</span>\n' +
+                '   <svg class="icon">\n' +
+                '       <use xlink:href="/assets/bootstrap-italia/dist/svg/sprite.svg#it-close-circle"></use>\n' +
+                '   </svg>\n' +
+                '</a>'
+            );
+
+        }
     }
 
     _handleFilterClick(evt) {
@@ -110,7 +161,7 @@ class Iniziative {
         const origin = btn.getAttribute('data-origin');
 
         // this._removeActiveClassFromChildren(btn.parentNode);
-        this._removeActiveClassFromChildren(options);
+     //   this._removeActiveClassFromChildren(options);
 //        document.querySelector("#collapse-" + origin).collapse("hide");
          $("#collapse-" + origin).collapse("hide");
 
@@ -118,27 +169,26 @@ class Iniziative {
         if (isActive) {
             btn.classList.remove('disabled');
             filterGroup = Shuffle.ALL_ITEMS;
+
+            var pos = SearchKey.indexOf(btnGroup);
+            SearchKey.splice(pos, 1);
+
         } else {
             var filtro = origin;
             if(origin == "activity") filtro = "attività";
 
             btn.classList.add('disabled');
-            document.querySelector('#selected-filter').innerHTML = "";
-            document.querySelector('#selected-filter').insertAdjacentHTML(
-                'afterbegin',
-                'Stai visualizzando soltanto le iniziative che hanno come ' + filtro + ': <b class="text-primary">' + btnGroup + '</b>\n' +
-                '<a class="read-more" href="#"  onclick="window.iniziative.removeLabels()">\n' +
-                '   <span class="text">Rimuovi il filtro</span>\n' +
-                '   <svg class="icon">\n' +
-                '       <use xlink:href="/assets/bootstrap-italia/dist/svg/sprite.svg#it-close-circle"></use>\n' +
-                '   </svg>\n' +
-                '</a>'
-            )
 
             filterGroup = btnGroup;
+            SearchKey.push(filterGroup);
         }
 
-        this.shuffle.filter(filterGroup);
+       // alert(SearchKey);
+        this.shuffle.filter(SearchKey);
+
+
+
+        this.stampaFiltri();
 
         //var elmnt = document.getElementById("grid");
         //elmnt.scrollIntoView({
@@ -153,6 +203,20 @@ class Iniziative {
         // }
         for (let i = parent.length - 1; i >= 0; i--) {
             parent[i].classList.remove('disabled');
+        }
+    }
+
+
+    _addActiveClass(parent) {
+        // const { children } = parent;
+        // for (let i = children.length - 1; i >= 0; i--) {
+        //     children[i].classList.remove('active');
+        // }
+        for (let i = parent.length - 1; i >= 0; i--) {
+            const btnGroup = parent[i].getAttribute('data-group')
+            if(SearchKey.includes(btnGroup)){
+                parent[i].classList.add('disabled');
+            }
         }
     }
 
